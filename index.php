4e551c945bea872ea86616562fcccef291b35ed5
CebZnfgreCUC <69229264+CebZnfgreCUC@users.noreply.github.com>
2020-08-05 08:38:52 +0500

    Create index.php
    Bosh sahifa

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<a href='http://t.me/Turgunboyev_Diyorbek'>Creator</a></br>";
+echo "<a href='http://t.me/OnlineWolf'>Admin</a></br>";
+echo "<a href='http://t.me/Inforte'>Admin</a></br>";
+?>
